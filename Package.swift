be0710756bd4ef5fba3f2a0a537e6c3702bfa4b3
Prashant Shrestha <prashantlurvs@gmail.com>
2020-05-20 17:16:05 +0545

    swift package dependency updated

--- Package.swift
+++ Package.swift
@@ -19,11 +19,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "UIPinInputView",
-            dependencies: [],
+            dependencies: ["DeviceX"],
             path: "Sources"),
         .testTarget(
             name: "UIPinInputViewTests",
-            dependencies: ["UIPinInputView"],
+            dependencies: ["UIPinInputView", "DeviceX"],
             path: "UIPinInputViewTests"),
     ]
 )
